e02f50e71a448745312a8ce1ebd9dafb8a562a6b
andrigamerita <37557992+andrigamerita@users.noreply.github.com>
2021-06-28 17:30:42 +0200

    Some tool improvements
    - Added sha check for the Station.nds rom included in releases
    - Not requiring a sha1 match for the input station rom (accepts any rom)
    - Automatically outputting to Patched.nds if -o is not specified
    - Automatically trying to use Station.nds in current directory
    - Help message improvement

--- dspatch/Program.cs
+++ dspatch/Program.cs
@@ -9,25 +9,27 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace dspatch
-{
-    class Program
-    {
-        static byte[] dsHash =
+namespace dspatch {
+    class Program {
+        static byte[] dsHashOriginal =
         {
-            0xF1, 0x8B, 0x55, 0xF3, 0xE1, 0x25, 0x9C, 0x03, 0xE1, 0x0D, 0x0E, 0xCB,
-            0x54, 0x96, 0x93, 0xB4, 0x29, 0x05, 0xCE, 0xB5
+            0xF1, 0x8B, 0x55, 0xF3, 0xE1, 0x25, 0x9C, 0x03, 0xE1, 0x0D, 0x0E, 0xCB, 0x54, 0x96, 0x93, 0xB4, 0x29, 0x05, 0xCE, 0xB5
+        };
+
+        static byte[] dsHashSlimdown =
+        {
+            0x10, 0x62, 0x86, 0x11, 0x88, 0xA4, 0x54, 0x46, 0x1F, 0xE4, 0x2F, 0x72, 0x3A, 0x4B, 0x51, 0x29, 0xF8, 0x65, 0xE4, 0xD8
         };
 
         static void PrintUsage()
         {
-            Console.WriteLine("Usage: dspatch -s download_station.nds -o result.nds [-i rom1.nds] [-I romfolder1]");
-            Console.WriteLine("You can add multiple roms by using -I or -i multiple times");
+            Console.WriteLine("Usage: dspatch [-s Station.nds] [-o Patched.nds] {-i ROM1.nds} {-I ROMsFolder1}");
+            Console.WriteLine("Note: You can add multiple ROMs by using -I or -i multiple times.");
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("== DS Download Station Patcher v1.0 ==");
+            Console.WriteLine("== DS Download Station Patcher v1.1 (OctoSpacc fork) ==");
             Console.WriteLine("Exploit by Gericom, shutterbug2000 and Apache Thunder\n");
 
             if (args.Length <= 1)
@@ -40,6 +42,7 @@ namespace dspatch
             string outPath = null;
             List<string> romPaths = new List<string>();
             string filePath;
+
             //parse arguments
             int q = 0;
             while (q < args.Length - 1)
@@ -87,15 +90,21 @@ namespace dspatch
 
             if (dsPath == null)
             {
-                Console.WriteLine("Error: Specify a download station rom!\n");
-                PrintUsage();
-                return;
+                if (File.Exists("Station.nds"))
+                {
+                    dsPath = "Station.nds";
+                }
+                else
+                {
+                    Console.WriteLine("Error: Specify a download station rom!\n(Station.nds not found in current directory)\n");
+                    PrintUsage();
+                    return;
+                }
             }
             if (outPath == null)
             {
-                Console.WriteLine("Error: Specify a destination path!\n");
-                PrintUsage();
-                return;
+                Console.WriteLine("Outputting to Patched.nds\n");
+                outPath = "Patched.nds";
             }
             if (romPaths.Count == 0)
             {
@@ -106,15 +115,19 @@ namespace dspatch
 
             byte[] dsdata = File.ReadAllBytes(dsPath);
             byte[] sha1 = SHA1.Create().ComputeHash(dsdata);
+            bool shaWarned = false;
+
             for (int i = 0; i < 20; i++)
             {
-                if (sha1[i] != dsHash[i])
+                if ((sha1[i] != dsHashOriginal[i]) && (sha1[i] != dsHashSlimdown[i]) && (!shaWarned))
                 {
-                    Console.WriteLine("Error: Invalid download station rom!");
-                    Console.WriteLine("The patcher is only compatible with:");
-                    Console.WriteLine("xxxx - DS Download Station - Volume 1 (Kiosk WiFi Demo Cart) (U)(Independent).nds");
-                    Console.WriteLine("SHA1: F18B55F3E1259C03E10D0ECB549693B42905CEB5");
-                    return;
+                    Console.WriteLine("WARNING: Invalid download station rom!");
+                    Console.WriteLine("The patcher is only tested with:");
+                    Console.WriteLine("- Station.nds, 745 KB (included in this tool's code repository)");
+                    Console.WriteLine("  SHA1: 1062861188A454461FE42F723A4B5129F865E4D8");
+                    Console.WriteLine("- xxxx - DS Download Station - Volume 1 (Kiosk WiFi Demo Cart) (U)(Independent).nds");
+                    Console.WriteLine("  SHA1: F18B55F3E1259C03E10D0ECB549693B42905CEB5");
+                    shaWarned = true;
                 }
             }
             DownloadStationPatcher p = new DownloadStationPatcher(new NDS(dsdata));
